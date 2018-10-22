ca5d276bcf6e99e2ea23ee19def83450ed7ed29e
netta <netta@reali.com>
2018-10-22 14:47:56 +0300

    final method

--- main.js
+++ main.js
@@ -19,23 +19,46 @@
 
 // main(100);
 
+// function main(n, counter = 0, sum = 0) {
+// 	function testRunner() {
+// 		window.requestAnimationFrame(() => {console.log('rerender')})
+// 		let t0 = performance.now();
+// 		setTimeout(() => {
+// 			let t1 = performance.now();
+// 			if (counter === n) {
+// 				console.log(`${counter}: ${sum/counter}`);
+// 				return;
+// 			}
+// 			sum += (t1-t0);
+// 			counter++
+// 			testRunner();
+// 		}, 0)
+// 	};
+// 	testRunner();
+// }
+
+// main(10);
+// main(1000);
+
+
 function main(n, counter = 0, sum = 0) {
-	function testRunner() {
+	let myInterval;
+	function myTester(n) {
 		let t0 = performance.now();
 		setTimeout(() => {
 			let t1 = performance.now();
-			if (counter === n) {
-				console.log(`${counter}: ${sum/counter}`);
-				return;
+			sum +=(t1-t0);
+			counter++;
+			if(counter === n) {
+				console.log(`${n}: ${sum / n}`);
+				clearInterval(myInterval);
 			}
-			sum += (t1-t0);
-			counter++
-			testRunner();
-		}, 0)
-	};
-	testRunner();
+		})
+	}
+	myInterval = setInterval(myTester, 50, n);
 }
 
 main(10);
 main(1000);
+
 
